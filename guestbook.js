dfee180efeb5164d9037b50083bd95abbc6de9e0
Kevin Koome <koome.kevo@gurucrafts.net>
2022-01-09 17:30:22 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7727eaaa.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
